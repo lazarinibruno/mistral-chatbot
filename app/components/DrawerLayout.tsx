2891e2950a7c1b8abc1a3f6ae467111995e33482
bruno <me@brunolazarini.ch>
2025-11-12 16:57:22 -0600

    feat: add DrawerLayout component skeleton

---
+++ app/components/DrawerLayout.tsx
@@ -0,0 +1,11 @@
+type DrawerLayoutProps = {
+  drawerId?: string;
+  navBarTitle?: string;
+  children: React.ReactNode;
+};
+
+export default function DrawerLayout({
+  drawerId = "drawer-1",
+  navBarTitle = "Navbar Title",
+  children,
+}: DrawerLayoutProps) {}
