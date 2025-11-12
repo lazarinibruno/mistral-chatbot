70098242594000429f16e48d11d1a58e68433d3d
bruno <me@brunolazarini.ch>
2025-11-12 17:16:16 -0600

    feat: add DrawerLayout component to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import DrawerLayout from "./components/DrawerLayout";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <DrawerLayout navBarTitle="Mistral Chatbot">
+        <p>Page Content</p>
+      </DrawerLayout>
+    </main>
+  );
 }
