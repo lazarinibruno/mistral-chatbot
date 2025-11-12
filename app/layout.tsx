dbfa312c1eb686ca50d51deaa642d6af57496042
bruno <me@brunolazarini.ch>
2025-11-12 13:12:21 -0600

    chore: set wireframe theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="wireframe">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
