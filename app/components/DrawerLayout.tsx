9d60b724c93bf1b4258970882a0b1036d4b34b37
bruno <me@brunolazarini.ch>
2025-11-12 17:14:32 -0600

    feat: add DrawerLayout content

--- app/components/DrawerLayout.tsx
+++ app/components/DrawerLayout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "./Navbar";
+import { Sidebar } from "./Sidebar";
+
 export type DrawerLayoutProps = {
   drawerId?: string;
   navBarTitle?: string;
@@ -8,4 +11,19 @@ export default function DrawerLayout({
   drawerId = "drawer-1",
   navBarTitle = "Navbar Title",
   children,
-}: DrawerLayoutProps) {}
+}: DrawerLayoutProps) {
+  return (
+    <div className="drawer lg:drawer-open">
+      {/* IMPORTANT: the input id must match the htmlFot in Navbar/Sidebar */}
+      <input id={drawerId} type="checkbox" className="drawer-toggle" />
+
+      <div className="drawer-content">
+        <Navbar drawerId={drawerId} title={navBarTitle} />
+
+        <div className="p-4">{children}</div>
+
+        <Sidebar drawerId={drawerId} />
+      </div>
+    </div>
+  );
+}
