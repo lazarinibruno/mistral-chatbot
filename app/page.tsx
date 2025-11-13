8224a3acb15ed391d241777440a61df1cfe5eb22
bruno <me@brunolazarini.ch>
2025-11-13 11:41:47 -0600

    feat: add test chat bubbles to DrawerLayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import { ChatBubble } from "./components/ChatBubble";
 import DrawerLayout from "./components/DrawerLayout";
 
 export default function Home() {
   return (
     <main>
       <DrawerLayout drawerId="app-drawer" navBarTitle="Mistral Chatbot">
-        <p>Page Content</p>
+        <div className="flex justify-center">
+          <div className="flex flex-col w-full max-w-2xl space-y-4">
+            <ChatBubble chatId={0} isUser={true}>
+              <p>Hi, how are you</p>
+            </ChatBubble>
+            <ChatBubble chatId={0} isUser={false}>
+              <p>Good, and you?</p>
+            </ChatBubble>
+          </div>
+        </div>
       </DrawerLayout>
     </main>
   );
