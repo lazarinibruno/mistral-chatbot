2b0052b4a1c5f35c7a55e0add5664203ef7f6c83
bruno <me@brunolazarini.ch>
2025-11-14 12:55:29 -0600

    feat: extract <div> containing chat bubbles to ChatLayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,14 @@
 import { ChatBubble } from "./components/ChatBubble";
 import DrawerLayout from "./components/DrawerLayout";
 import { ChatInput } from "./components/ChatInput";
+import { ChatLayout } from "./components/ChatLayout";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col min-h-screen">
       {/* Header / Drawer */}
       <DrawerLayout drawerId="app-drawer" navBarTitle="Mistral Chatbot">
-        {/* Chat area - scrollable */}
-        <div className="flex-1 overflow-y-auto pb-24">
-          <div className="flex justify-center">
-            <div className="flex flex-col w-full max-w-2xl space-y-4 p-4">
-              <ChatBubble chatId={0} isUser={true}>
-                <p>Hi, how are you</p>
-              </ChatBubble>
-              <ChatBubble chatId={0} isUser={false}>
-                <p>Good, and you?</p>
-              </ChatBubble>
-            </div>
-          </div>
-        </div>
-
+        <ChatLayout />
         {/* Input field - fixed to bottom */}
         <div className="fixed inset-x-0 bottom-4 p-4">
           <div className="mx-auto w-full max-w-2xl">
