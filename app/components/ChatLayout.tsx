bd8c25c0c2f5cef4ccdca9a6d36e444c799465bb
bruno <me@brunolazarini.ch>
2025-11-20 13:50:37 -0600

    feat: adapt the ChatBubble components to account for the API response

--- app/components/ChatLayout.tsx
+++ app/components/ChatLayout.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import { useChatContext } from "../contexts/ChatContext";
+import { chatMsg } from "../types/app-types";
 import { ChatBubble } from "./ChatBubble";
 
 export function ChatLayout() {
+  const { chats } = useChatContext();
+
   return (
     <div className="flex-1 overflow-y-auto pb-24">
       <div className="flex justify-center">
         <div className="flex flex-col w-full max-w-2xl space-y-4 p-4">
-          <ChatBubble chatId={0} isUser={true}>
-            <p>Hi, how are you</p>
-          </ChatBubble>
-          <ChatBubble chatId={0} isUser={false}>
-            <p>Good, and you?</p>
-          </ChatBubble>
+          {chats.map((chat) => (
+            <ChatBubble chatId={chat.id} role={chat.role} key={chat.id}>
+              {chat.content}
+            </ChatBubble>
+          ))}
         </div>
       </div>
     </div>
