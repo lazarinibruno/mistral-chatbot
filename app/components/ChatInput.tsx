2dd79fe0577b19d7dc14423a2e7760b179025cf1
bruno <me@brunolazarini.ch>
2025-11-20 14:38:57 -0600

    feat: add logic to interact with API and send user prompts and get responses

--- app/components/ChatInput.tsx
+++ app/components/ChatInput.tsx
@@ -1,38 +1,73 @@
 "use client";
 
 import React, { useState } from "react";
+import { ApiResponse } from "../types/api-types";
+import { chatMsg } from "../types/app-types";
+import { generateMessageId } from "../utilities/generateMessageId";
+import { useChatContext } from "../contexts/ChatContext";
 
-type message = { content: string; role: string };
+type message = { content: string; role: "user" | "assistant" };
 
 export function ChatInput() {
-  const [msg, setMsg] = useState("");
+  const [usrInput, setUsrInput] = useState("");
 
-  const handleSumbmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const { chats, addChatMsg: addChatMsg } = useChatContext();
 
-    // if (!msg.trim()) return; // Prevents user from submiting whitespaces
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // Prevent reloading of the page when user user submits an input
+    if (!usrInput.trim()) return; // Prevent the user from sending whitespaces
 
     send();
 
-    setMsg("");
+    setUsrInput("");
   };
 
   const send = async () => {
-    const mssg: message = { content: "how are you?", role: "user" };
+    // Message type needed for the API
+    const msg: message = {
+      content: usrInput,
+      role: "user",
+    };
+
+    // Message type needed for the rendering of the chat bubbles
+    const userMsg: chatMsg = {
+      id: generateMessageId(),
+      content: msg.content,
+      role: msg.role,
+    };
+
+    // Here, there are two operations: an enqueue operation that runs async and updates the state of the messages.
+    // However, since it is async, it may not be completed in time before the POST is made, so we need to construct an
+    // array locally to send.
+
+    addChatMsg(userMsg); // async
+
+    // The chat completion endpoint is stateless, so sending the whole conversation is needed
+    const apiMsgs: message[] = [...chats, msg].map((m) => ({
+      content: m.content,
+      role: m.role,
+    })) as message[];
 
     try {
       const response = await fetch("/api/chat", {
         method: "POST",
-        headers: { "Conent-Type": "application/json" },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           model: "mistral-small-latest",
-          messages: [mssg],
+          messages: apiMsgs,
         }),
       });
 
-      const body = await response.json();
+      const body: ApiResponse = await response.json();
 
-      console.log(body);
+      body.choices.forEach((choice) => {
+        const chatMsg: chatMsg = {
+          id: generateMessageId(),
+          content: choice.message.content,
+          role: choice.message.role,
+        };
+        addChatMsg(chatMsg);
+      });
     } catch (e) {
       console.error(e);
     }
@@ -40,14 +75,14 @@ export function ChatInput() {
 
   return (
     <form
-      onSubmit={handleSumbmit}
+      onSubmit={handleSubmit}
       className="flex w-full max-w-2xl mx-auto space-x-2"
     >
       <input
         type="text"
         placeholder="Ask anything"
-        value={msg}
-        onChange={(e) => setMsg(e.target.value)}
+        value={usrInput}
+        onChange={(e) => setUsrInput(e.target.value)}
         className="input flex-1"
       />
 
