edfe692a3a04236ff4eb8bcb1258aacf289718d4
bruno <me@brunolazarini.ch>
2025-11-20 13:39:56 -0600

    feat: extract dynamic rendering logic of the chat bubble type to an if statement.

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -2,13 +2,21 @@ import React from "react";
 
 interface ChatBubbleProps {
   chatId: number;
-  isUser: boolean;
+  role: string;
   children: React.ReactNode;
 }
 
-export function ChatBubble({ chatId, isUser, children }: ChatBubbleProps) {
+export function ChatBubble({ chatId, role, children }: ChatBubbleProps) {
+  // For some reason that I have not figured out, the dynamic rendering of the chat bubbles only works by doing this
+  // if statement instead of simply: className={chat chat-${(role === "user" ? "end" : "start")}}.
+  let bubbleType = "chat-end";
+
+  if (role === "assistant") {
+    bubbleType = "chat-start";
+  }
+
   return (
-    <div className={`chat chat-${isUser ? "end" : "start"}`}>
+    <div className={`chat ${bubbleType}`}>
       <div key={chatId} className="chat-bubble">
         {children}
       </div>
