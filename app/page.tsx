0e6e5187e912ef80602f43b55f5498c2584a4965
bruno <me@brunolazarini.ch>
2025-11-20 13:37:46 -0600

    feat: wrap app in the chat provider

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,25 @@ import { ChatBubble } from "./components/ChatBubble";
 import DrawerLayout from "./components/DrawerLayout";
 import { ChatInput } from "./components/ChatInput";
 import { ChatLayout } from "./components/ChatLayout";
+import { ChatProvider } from "./contexts/ChatContext";
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col min-h-screen">
-      {/* Header / Drawer */}
-      <DrawerLayout drawerId="app-drawer" navBarTitle="Mistral Chatbot">
-        <ChatLayout />
-        {/* Input field - fixed to bottom */}
-        <div className="fixed inset-x-0 bottom-4 p-4">
-          <div className="mx-auto w-full max-w-2xl">
-            <div className="w-full max-w-2xl">
-              <ChatInput />
+    <ChatProvider>
+      <main className="relative flex flex-col min-h-screen">
+        {/* Header / Drawer */}
+        <DrawerLayout drawerId="app-drawer" navBarTitle="Mistral Chatbot">
+          <ChatLayout />
+          {/* Input field - fixed to bottom */}
+          <div className="fixed inset-x-0 bottom-4 p-4">
+            <div className="mx-auto w-full max-w-2xl">
+              <div className="w-full max-w-2xl">
+                <ChatInput />
+              </div>
             </div>
           </div>
-        </div>
-      </DrawerLayout>
-    </main>
+        </DrawerLayout>
+      </main>
+    </ChatProvider>
   );
 }
