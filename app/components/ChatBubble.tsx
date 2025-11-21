d67a15ba5e1f6192190792b40677e28e819da48c
bruno <me@brunolazarini.ch>
2025-11-21 12:14:09 -0600

    refactor: simplify if statement

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -9,11 +9,7 @@ interface ChatBubbleProps {
 export function ChatBubble({ chatId, role, children }: ChatBubbleProps) {
   // For some reason that I have not figured out, the dynamic rendering of the chat bubbles only works by doing this
   // if statement instead of simply: className={chat chat-${(role === "user" ? "end" : "start")}}.
-  let bubbleType = "chat-end";
-
-  if (role === "assistant") {
-    bubbleType = "chat-start";
-  }
+  let bubbleType = role === "user" ? "chat-end" : "chat-start";
 
   return (
     <div className={`chat ${bubbleType}`}>
