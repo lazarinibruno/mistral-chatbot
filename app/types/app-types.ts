e10752ce69397d1e7b9170d5f920b7247d94f683
bruno <me@brunolazarini.ch>
2025-11-21 11:09:58 -0600

    docs

--- app/types/app-types.ts
+++ app/types/app-types.ts
@@ -1,3 +1,11 @@
+
+/**
+ * @typedef {Object} chatMsg - An internal type that has a unique id for each message
+ * 
+ * @property {number} id - Unique identifier
+ * @property {string} content - The text of the message
+ * @property {string} role - The sender of the message; either "user" or "assistant"
+ */
 export type chatMsg = {
   id: number;
   content: string;
