aff64ad13f2ca726427ec317ab4049a2c1f58c62
bruno <me@brunolazarini.ch>
2025-11-24 18:29:29 -0600

    feat: add LoadingDots

--- app/components/ChatLayout.tsx
+++ app/components/ChatLayout.tsx
@@ -3,19 +3,24 @@
 import { useChatContext } from "../contexts/ChatContext";
 import { chatMsg } from "../types/app-types";
 import { ChatBubble } from "./ChatBubble";
+import LoadingDots from "./LoadingDots";
 
 export function ChatLayout() {
-  const { chats } = useChatContext();
+  const { chats, isResponseLoading } = useChatContext();
 
   return (
     <div className="flex-1 overflow-y-auto pb-24">
       <div className="flex justify-center">
         <div className="flex flex-col w-full max-w-2xl space-y-4 p-4">
-          {chats.map((chat) => (
-            <ChatBubble chatId={chat.id} role={chat.role} key={chat.id}>
-              {chat.content}
-            </ChatBubble>
-          ))}
+          <div>
+            {chats.map((chat, i) => (
+              <ChatBubble chatId={chat.id} role={chat.role} key={i}>
+                {chat.content}
+              </ChatBubble>
+            ))}
+          </div>
+
+          <div>{isResponseLoading() && <LoadingDots />}</div>
         </div>
       </div>
     </div>
