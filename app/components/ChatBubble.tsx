01caeda170312c0b20cfa0a006370fc84eee77cb
bruno <me@brunolazarini.ch>
2025-11-24 18:37:56 -0600

    feat: add more padding to the chat bubbles

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -12,7 +12,7 @@ export function ChatBubble({ chatId, role, children }: ChatBubbleProps) {
   let bubbleType = role === "user" ? "chat-end" : "chat-start";
 
   return (
-    <div className={`chat ${bubbleType}`}>
+    <div className={`chat ${bubbleType} py-2`}>
       <div key={chatId} className="chat-bubble">
         {children}
       </div>
