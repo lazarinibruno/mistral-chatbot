67f793a9f88ef47008e03dc094c70fb5fa1a4126
bruno <me@brunolazarini.ch>
2025-11-25 08:46:11 -0600

    feat: adjust chat bubbles width

--- app/components/ChatLayout.tsx
+++ app/components/ChatLayout.tsx
@@ -10,7 +10,7 @@ export function ChatLayout() {
   return (
     <div className="flex-1 overflow-y-auto pb-24">
       <div className="flex justify-center">
-        <div className="flex flex-col w-full max-w-4xl space-y-4 p-4">
+        <div className="flex flex-col w-full max-w-3xl space-y-4 p-4">
           <div>
             {chats.map((chat, i) => (
               <ChatBubble chatId={chat.id} role={chat.role} key={i}>
