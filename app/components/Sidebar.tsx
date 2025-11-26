79fa9366f4fe188b058c04a1e6a5bedda09b6dab
bruno <me@brunolazarini.ch>
2025-11-26 11:50:53 -0600

    feat: extract icon to component and add logic to have conversations show up in the sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { HomeIcon } from "../assets/HomeIcon";
+import { SettingsIcon } from "../assets/SettingsIcon";
+import { useChatContext } from "../contexts/ChatContext";
+import { ChatButton } from "./ChatButton";
+
 export type SidebarProps = {
   drawerId: string;
 };
 
 export function Sidebar({ drawerId }: SidebarProps) {
+  const { convos } = useChatContext();
+
   return (
     <div className="drawer-side is-drawer-close:overflow-visible">
       {/* clicking the overlay will toggle the checkbox (close drawer on mobile) */}
@@ -13,53 +22,44 @@ export function Sidebar({ drawerId }: SidebarProps) {
       ></label>
 
       <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
-        <ul className="menu w-full grow">
-          <li>
-            <button
-              className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-              data-tip="Homepage"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2"
-                fill="none"
-                stroke="currentColor"
-                className="my-1.5 inline-block size-4"
+        <div className="flex flex-col">
+          <ul className="menu w-full grow">
+            <li>
+              <button
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="Homepage"
               >
-                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-              </svg>
-              <span className="is-drawer-close:hidden">Homepage</span>
-            </button>
-          </li>
+                <HomeIcon />
+                <span className="is-drawer-close:hidden">Homepage</span>
+              </button>
+            </li>
 
-          <li>
-            <button
-              className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-              data-tip="Settings"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2"
-                fill="none"
-                stroke="currentColor"
-                className="my-1.5 inline-block size-4"
+            <li>
+              <button
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="Settings"
               >
-                <path d="M20 7h-9"></path>
-                <path d="M14 17H5"></path>
-                <circle cx="17" cy="17" r="3"></circle>
-                <circle cx="7" cy="7" r="3"></circle>
-              </svg>
-              <span className="is-drawer-close:hidden">Settings</span>
-            </button>
-          </li>
-        </ul>
+                <SettingsIcon />
+                <span className="is-drawer-close:hidden">Settings</span>
+              </button>
+            </li>
+          </ul>
+
+          <div className="flex flex-col pl-4 is-drawer-close:hidden">
+            <p className="pb-2">Your chats</p>
+            <ul className="list space-y-2">
+              {convos.map((convo) => (
+                <li>
+                  <ChatButton
+                    key={convo.id}
+                    id={convo.id}
+                    title={convo.title}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
