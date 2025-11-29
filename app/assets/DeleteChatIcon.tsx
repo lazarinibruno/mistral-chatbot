2406f01372c5285db73aa4d131a72351c0551ee5
bruno <me@brunolazarini.ch>
2025-11-29 17:12:04 -0600

    feat: add delete chat icon

---
+++ app/assets/DeleteChatIcon.tsx
@@ -0,0 +1,19 @@
+export const DeleteChatIcon = () => {
+  return (
+    <svg
+      width="14.461231"
+      height="14.59073"
+      viewBox="0 0 14.461231 14.59073"
+      version="1.1"
+      id="svg1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+    >
+      <defs id="defs1" />
+      <g id="layer1" transform="translate(-11.072806,-35.261539)">
+        <path d="m 20.427954,36.973637 v 8.974584 m -4.174443,-8.974584 v 8.974584 m 2.129913,-8.974584 V 46.98216 m -0.05928,-11.456038 h -3.163851 v 1.447515 m 3.223131,-1.447515 h 3.163851 v 1.447515 m -10.209886,0 h 1.778374 m 10.375316,0 h 1.778374 m -11.993685,0 H 23.491079 V 49.587685 H 13.275768 Z" />
+      </g>
+    </svg>
+  );
+};
